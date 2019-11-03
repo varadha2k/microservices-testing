2bad9ef9ac8008e69019cdd30da0886e157e15a5
Varadharajan Srinivasan <varadharajan@gmail.com>
2019-11-03 21:08:56 +0530

    reverted changes in weather response

--- src/main/java/com/varadha2k/weather/WeatherResponse.java
+++ src/main/java/com/varadha2k/weather/WeatherResponse.java
@@ -13,6 +13,10 @@ public class WeatherResponse {
         this.currently = new Currently(currenticon);
     }
 
+    public Currently getCurrently() {
+        return currently;
+    }
+
     public String geticon() {
         return currently.geticon();
     }
@@ -27,6 +31,17 @@ public class WeatherResponse {
         return currently != null ? currently.equals(response.currently) : response.currently == null;
     }
 
+    @Override
+    public int hashCode() {
+        return currently != null ? currently.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "WeatherResponse{" +
+                "currently=" + currently +
+                '}';
+    }
 
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Currently {
@@ -42,5 +57,26 @@ public class WeatherResponse {
             return icon;
         }
 
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Currently currently = (Currently) o;
+
+            return icon != null ? icon.equals(currently.icon) : currently.icon == null;
+        }
+
+        @Override
+        public int hashCode() {
+            return icon != null ? icon.hashCode() : 0;
+        }
+
+        @Override
+        public String toString() {
+            return "Currently{" +
+                    "icon='" + icon + '\'' +
+                    '}';
+        }
     }
 }
