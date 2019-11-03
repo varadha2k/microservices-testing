76ca1891e65190f2fdc12c1f5e33adde6b485fef
Varadharajan Srinivasan <varadharajan@gmail.com>
2019-11-03 22:58:55 +0530

    code coverage for weatherclient

--- src/test/java/com/varadha2k/weather/WeatherResponseTest.java
+++ src/test/java/com/varadha2k/weather/WeatherResponseTest.java
@@ -1,13 +1,13 @@
 package com.varadha2k.weather;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.varadha2k.helper.FileLoader;
-import com.varadha2k.weather.WeatherResponse;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.varadha2k.helper.FileLoader;
 
 public class WeatherResponseTest {
 
@@ -15,9 +15,13 @@ public class WeatherResponseTest {
     public void shouldDeserializeJson() throws Exception {
         String jsonResponse = FileLoader.read("classpath:weatherApiResponse.json");
         WeatherResponse expectedResponse = new WeatherResponse("Very Cloudy");
-
         WeatherResponse parsedResponse = new ObjectMapper().readValue(jsonResponse, WeatherResponse.class);
 
         assertThat(parsedResponse, is(expectedResponse));
+        assertNotNull(expectedResponse.getCurrently());
+        assertNotNull(expectedResponse.hashCode());
+        assertNotNull(expectedResponse.getCurrently().hashCode());
+        assertNotNull(expectedResponse.getCurrently().toString());
+        assertNotNull(expectedResponse.toString());
     }
 }
