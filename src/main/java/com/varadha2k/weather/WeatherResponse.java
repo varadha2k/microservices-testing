e5b8bb093965962472b87da103b3a927bcd22a28
Varadharajan Srinivasan <varadharajan@gmail.com>
2019-11-03 21:33:10 +0530

    remove dead code

--- src/main/java/com/varadha2k/weather/WeatherResponse.java
+++ src/main/java/com/varadha2k/weather/WeatherResponse.java
@@ -13,22 +13,8 @@ public class WeatherResponse {
         this.currently = new Currently(currenticon);
     }
 
-    public Currently getCurrently() {
-        return currently;
-    }
-
     public String geticon() {
         return currently.geticon();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        WeatherResponse response = (WeatherResponse) o;
-
-        return currently != null ? currently.equals(response.currently) : response.currently == null;
     }
 
     @Override
@@ -36,12 +22,6 @@ public class WeatherResponse {
         return currently != null ? currently.hashCode() : 0;
     }
 
-    @Override
-    public String toString() {
-        return "WeatherResponse{" +
-                "currently=" + currently +
-                '}';
-    }
 
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Currently {
@@ -67,16 +47,5 @@ public class WeatherResponse {
             return icon != null ? icon.equals(currently.icon) : currently.icon == null;
         }
 
-        @Override
-        public int hashCode() {
-            return icon != null ? icon.hashCode() : 0;
-        }
-
-        @Override
-        public String toString() {
-            return "Currently{" +
-                    "icon='" + icon + '\'' +
-                    '}';
-        }
     }
 }
